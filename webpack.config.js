b585a6d3fd112777b87d0f088458dfe39f499cbd
ako-d <akitinexxx@gmail.com>
2025-07-03 14:52:44 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,25 @@
-const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 const slsw = require('serverless-webpack');
 
 module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  externals: [nodeExternals()],
+  mode: 'production',
+  resolve: {
+    extensions: ['.js', '.json', '.ts'], // ここで.tsを認識させる
+  },
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
+        test: /\.ts$/,
+        loader: 'ts-loader',
         exclude: /node_modules/,
-      },
-      {
-        test: /\.jsx?$/,
-        include: __dirname,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
       },
     ],
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  output: {
+    libraryTarget: 'commonjs2',
+    path: path.resolve(__dirname, '.webpack'),
+    filename: '[name].js',
   },
 };
