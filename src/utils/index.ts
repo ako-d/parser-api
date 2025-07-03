ae0330f80336e92c4841ce6f40ec2478c19f0064
ako-d <akitinexxx@gmail.com>
2025-07-03 14:26:38 +0900

    Update index.ts

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,27 @@
-export const dummy = () => {
-  return "dummy utils";
+export const corsSuccessResponse = (body) => ({
+  statusCode: 200,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
+  body: JSON.stringify(body),
+});
+
+export const corsErrorResponse = (body) => ({
+  statusCode: 500,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
+  body: JSON.stringify(body),
+});
+
+export const runWarm = (lambda) => {
+  return (event, context, callback) => {
+    // Detect the "warming" event from serverless-plugin-warmup
+    if (event.source === 'serverless-plugin-warmup') {
+      return callback(null, 'Lambda is warm!');
+    }
+    return lambda(event, context, callback);
+  };
 };
